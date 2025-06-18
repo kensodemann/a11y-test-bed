a932be58ee63668b7fbf74a98d1a6b2455ea84a0
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-06-18 11:27:05 -0500

    add capacitor

---
+++ android/app/src/main/java/io/ionic/a11ytestbed/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ionic.a11ytestbed;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.a11ytestbed',
+  appName: 'a11y-test-bed',
+  webDir: 'dist/a11y-test-bed/browser',
+};
+
+export default config;
